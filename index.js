03741e48c9178435b403c47f80e057de98437af9
Diego Rubin <diegorubin@ciandt.com>
2016-03-10 08:52:51 -0300

    listing templates.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var dot = require('dot');
+var dot = require('dot').process({path: "./views"});;
 var path = require('path');
 var bodyParser = require('body-parser');
 var url = require('url');
@@ -19,102 +19,117 @@ app.use(express.static(path.join(__dirname, 'public')));
 /* mongo configuration */
 var db = monk('localhost:27017/nscms');
 app.use(function(req,res,next){
-    req.db = db;
-    next();
+  req.db = db;
+  next();
 });
 
 /* actions */
 app.post('/api/templates', function(req, res) {
-    var template = req.body;
-    var db = req.db;
-    var collection = db.get('templates');
+  var template = req.body;
+  var db = req.db;
+  var collection = db.get('templates');
 
-    // saving in mongo
-    collection.remove({key: template.key});
-    collection.insert(template, function(err, doc){
-      if(err) { console.log(err); }
-      else {console.log('saving template: ' + template.key);}
-    });
+  // saving in mongo
+  collection.remove({key: template.key});
+  collection.insert(template, function(err, doc){
+    if(err) { console.log(err); }
+    else {console.log('saving template: ' + template.key);}
+  });
 
 
-    res.status(200).send('ok');
+  res.status(200).send('ok');
 });
 
 app.post('/api/partials', function(req, res) {
-    var partial = req.body;
-    var db = req.db;
-    var collection = db.get('partials');
+  var partial = req.body;
+  var db = req.db;
+  var collection = db.get('partials');
 
-    // saving in mongo
-    collection.remove({key: partial.key});
-    collection.insert(partial, function(err, doc){
-      if(err) { console.log(err); }
-      else {console.log('saving partial: ' + partial.key);}
-    });
+  // saving in mongo
+  collection.remove({key: partial.key});
+  collection.insert(partial, function(err, doc){
+    if(err) { console.log(err); }
+    else {console.log('saving partial: ' + partial.key);}
+  });
 
 
-    res.status(200).send('ok');
+  res.status(200).send('ok');
+});
+
+app.get("/templates", function(req,res){
+  var db = req.db;
+
+  db.get('templates').find({}, {}, function(err, templates){
+    if(err) {
+      console.log(err);
+      res.status(500);
+    } else {
+      res.status(200).send(dot.templates({templates: templates}))
+    }
+
+  });
+
 });
 
 app.get('/:template', function(req, res){
-    var db = req.db;
-    var key = req.params.template;
+  var db = req.db;
+  var key = req.params.template;
 
-    var url_parts = url.parse(req.url, true);
-    var data = url_parts.query;
-    data['title'] = data['title'] || 'Título';
-    data['body'] = data['body'] || 'corpo';
+  var url_parts = url.parse(req.url, true);
+  var data = url_parts.query;
+  data['title'] = data['title'] || 'Título';
+  data['body'] = data['body'] || 'corpo';
 
-    db.get('templates').find({key: key}, {}, function(err, templates){
-      if (!err && templates[0]) {
-        var template = templates[0];
+  db.get('templates').find({key: key}, {}, function(err, templates){
+    if (!err && templates[0]) {
+      var template = templates[0];
 
-        // recover partials
-        var partial_keys = [];
+      // recover partials
+      var partial_keys = [];
 
-        var re = /\{\{#def.([\w_]+)\}\}/g;
-        var def = {};
-        var m;
+      var re = /\{\{#def.([\w_]+)\}\}/g;
+      var def = {};
+      var m;
 
-        do {
-            m = re.exec(template.content);
-            if (m) {
-              partial_keys.push(m[1]);
-            }
-        } while (m);
-
-        db.get('partials').find({key: {$in: partial_keys}}, {}, function(err, partials){
-
-          for(var partial in partials) {
-            def[partials[partial].key] = partials[partial].content;
-            console.log('loading partial: ' + partials[partial].key);
+      do {
+          m = re.exec(template.content);
+          if (m) {
+            partial_keys.push(m[1]);
           }
+      } while (m);
 
-          var render = dot.compile(template.content, def);
-          var result = render(data);
+      db.get('partials').find({key: {$in: partial_keys}}, {}, function(err, partials){
 
-          console.log('rendering template: ' + key);
-          res.status(200).send(result);
-        });
+        for(var partial in partials) {
+          def[partials[partial].key] = partials[partial].content;
+          console.log('loading partial: ' + partials[partial].key);
+        }
 
-      } else {
-        res.status(404).send('not found');
-      }
-    });
+        var render = dot.compile(template.content, def);
+        var result = render(data);
+
+        console.log('rendering template: ' + key);
+        res.status(200).send(result);
+      });
+
+    } else {
+      res.status(404).send('not found');
+    }
+  });
 });
 
 app.get('/partials/:partial', function(req, res){
-    var db = req.db;
-    var key = req.params.partial;
+  var db = req.db;
+  var key = req.params.partial;
 
-    db.get('partials').find({key: key}, {}, function(err, partials){
-      if (!err && partials[0]) {
-        var partial = partials[0];
-        res.status(200).send(partial);
-      } else {
-        res.status(404).send(result);
-      }
-    });
+  db.get('partials').find({key: key}, {}, function(err, partials){
+    if (!err && partials[0]) {
+      var partial = partials[0];
+      res.status(200).send(partial);
+    } else {
+      res.status(404).send(result);
+    }
+  });
 });
 
 app.listen(port);
---
+++ views/templates.js
@@ -0,0 +1,9 @@
+(function(){function templates(it) {
+var out='<!DOCTYPE html><html lang=\'pt-BR\'> <head> <title>Edição de Template</title> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous"> </head> <body> <h1>Templates</h1> <table> <tbody> ';var arr1=it.templates;if(arr1){var template,index=-1,l1=arr1.length-1;while(index<l1){template=arr1[index+=1];out+=' <tr> <td>';template.keyout+='</td> <td>';template.contentout+='</td> </tr> ';} } out+=' </tbody> </table> </body></html>';return out;
+}var itself=templates, _encodeHTML=(function (doNotSkipEncoded) {
+		var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': "&#34;", "'": "&#39;", "/": "&#47;" },
+			matchHTML = doNotSkipEncoded ? /[&<>"'\/]/g : /&(?!#?\w+;)|<|>|"|'|\//g;
+		return function(code) {
+			return code ? code.toString().replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : "";
+		};
+	}());if(typeof module!=='undefined' && module.exports) module.exports=itself;else if(typeof define==='function')define(function(){return itself;});else {window.render=window.render||{};window.render['templates']=itself;}}());
